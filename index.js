1b192d5d2b3b91cf89e3610fdfaea53bbfa640da
Kwanwoo Baek <kwanwoob@usc.edu>
2022-08-14 14:49:51 -0600

    Main Screen

---
+++ index.js
@@ -0,0 +1,31 @@
+document.querySelector("#icon").onmouseenter = function() {
+    this.src = "images/logo-2.png";
+}
+
+document.querySelector("#icon").onmouseleave = function() {
+    this.src = "images/logo.png";
+}
+
+document.querySelector("#linkedin").onmouseenter = function() {
+    this.src = "images/linkedin-2.png";
+}
+
+document.querySelector("#linkedin").onmouseleave = function() {
+    this.src = "images/linkedin.png";
+}
+
+document.querySelector("#github").onmouseenter = function() {
+    this.src = "images/github-2.png";
+}
+
+document.querySelector("#github").onmouseleave = function() {
+    this.src = "images/github.png";
+}
+
+document.querySelector("#email").onmouseenter = function() {
+    this.src = "images/email-2.png";
+}
+
+document.querySelector("#email").onmouseleave = function() {
+    this.src = "images/email.png";
+}
