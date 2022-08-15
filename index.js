d42f293a1c3d81f5138e4628acd0563004338dad
Kwanwoo Baek <kwanwoob@usc.edu>
2022-08-15 01:13:31 -0600

    Almost there
    Still need to add skills, activities, and provide links for projects

--- index.js
+++ index.js
@@ -29,3 +29,19 @@ document.querySelector("#email").onmouseenter = function() {
 document.querySelector("#email").onmouseleave = function() {
     this.src = "images/email.png";
 }
+
+document.querySelector("#arrow").onmouseenter = function() {
+    this.src = "images/arrow-2.png";
+}
+
+document.querySelector("#arrow").onmouseleave = function() {
+    this.src = "images/arrow.png";
+}
+
+document.querySelector("#arrowup").onmouseenter = function() {
+    this.src = "images/arrowup-2.png";
+}
+
+document.querySelector("#arrowup").onmouseleave = function() {
+    this.src = "images/arrowup.png";
+}
